79c12a2e3e004ceb8ba4bf23bf32180d69f50c39
Jordi_lumbreras <jordilumbrerasaragon@a.cifpperedesongall.org>
2025-01-24 18:36:18 +0100

    Primer ejercicio Python

--- Activitat Avaluable/Llibre.py
+++ Activitat Avaluable/Llibre.py
@@ -7,20 +7,23 @@ class Llibre:
         self.estanteria = estanteria
         self.fila = fila
 
+
     def __str__(self):
         return(f"Nom: {self.titol}, Autor: {self.autor}, Pàgines: {self.num_pag}, "
                f"En Préstec: {'{Sí' if self.en_prestec else 'No'}, "
                f"Estanteria: {self.estanteria}, Fila: {self.fila}")
 
+
 def mostrar_menu():
     print("\nInventari de la biblioteca CIFP PERE DE SON GALL")
-    print("1. Afegir un nou llibre a l'inventari")
-    print("2. Eliminar un llibre per títol")
-    print("3. Eliminar un llibre per estanteria i fila")
-    print("4. Mostrar llibre per títol")
-    print("5. Mostrar llibre per autor")
-    print("6. Mostrar tota la llista de llibres")
-    print("7. Sortir")
+    print(" === 1. Afegir un nou llibre a l'inventari")
+    print(" === 2. Eliminar un llibre per títol")
+    print(" === 3. Eliminar un llibre per estanteria i fila")
+    print(" === 4. Mostrar llibre per títol")
+    print(" === 5. Mostrar llibre per autor")
+    print(" === 6. Mostrar tota la llista de llibres")
+    print(" === 7. Sortir")
+
 
 def afegir_llibre(inventari):
     titol = input("Títol del llibre: ")
@@ -33,7 +36,8 @@ def afegir_llibre(inventari):
     inventari.append(llibre)
     print("Llibre afegir correctament.")
 
-def eliminar_llibre_titol(inventari):
+
+def eliminar_llibre_per_titol(inventari):
     nom = input("Nom del llibre: ")
     for llibre in inventari:
         if llibre.titol == nom:
@@ -42,17 +46,44 @@ def eliminar_llibre_titol(inventari):
             return
     print("No s'ha trobat cap llibre amb aquest nom.")
 
-def eliminar_llibre_estanteria_fila(inventari):
 
-def mostrar_llibre(inventari):
+def eliminar_llibre_estanteria_fila(inventari):
+    estanteria = input("Estanteria: ")
+    fila = input("Fila: ")
+    for llibre in inventari:
+        if llibre.estanteria == estanteria and llibre.fila == fila:
+            inventari.remove(llibre)
+            print("Llibre eliminar correctament")
+            return
+    print("No s'ha trobat cap llibre en aquesta estanteria.")
+
 
 def mostrar_llibre_titol(inventari):
+    nom = input("Títol del llibre: ")
+    for llibre in inventari:
+        if llibre.nom == nom:
+            print(llibre)
+            return
+    print("No s'ha trobat cap llibre amb aquest nom.")
+
 
 def mostrar_llibre_autor(inventari):
+    autor = input("Nom del autor: ")
+    llibres_autor = [llibre for llibre in inventari if llibre.autor == autor]
+    if llibres_autor:
+        for llibre in llibres_autor:
+            print(llibre)
+    else:
+        print("No s'ha trobat cap llibre amb aquest nom d'autor.")
+
 
 def mostrar_llista_llibres(inventari):
+    if inventari:
+        for llibre in inventari:
+            print(llibre)
+    else:
+        print("No hi ha cap llibre afegit.")
 
-def sortir(inventari):
 
 def main():
     inventari = []
@@ -62,7 +93,7 @@ def main():
         if opcio == 1:
             afegir_llibre(inventari)
         elif opcio == 2:
-            eliminar_llibre_titol(inventari)
+            eliminar_llibre_per_titol(inventari)
         elif opcio == 3:
             eliminar_llibre_estanteria_fila(inventari)
         elif opcio == 4:
